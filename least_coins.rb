7dd7f30ade3f19901a5e0af6f34f920ab56cf967
ETSGitHub <etschulte@gmail.com>
2018-07-26 17:40:11 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,17 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
+dollar_val = cents / 100
+diff0 = cents -(dollar_val *100)
+quarter_val = diff0 / 25
+diff1 = cents - (quarter_val * 25)
+dime_val = diff1 / 10
+diff2 = diff1 - (dime_val * 10)
+nickel_val = diff2 / 5
+diff3 = diff2 - (nickel_val * 5)
+penny_val = diff3 / 1
+least = {:dollar => dollar_val, :quarter => quarter_val, :dime => dime_val, :nickel => nickel_val, :penny => penny_val}
+puts least
 end
+
+least_coins(257)
